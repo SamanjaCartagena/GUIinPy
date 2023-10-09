ada214affc20435007c3e6b39450929ce8057062
Samanja Cartagena <c.samanja09@Pabs.local>
2023-10-09 10:04:54 -0400

    browse button finished

--- pdftotext/app.py
+++ pdftotext/app.py
@@ -5,9 +5,33 @@ Created on Mon Oct  9 01:26:11 2023
 @author: c.samanja09
 """
 import tkinter as tk
+
 import PyPDF2
 from PIL import Image,ImageTk
 
 root = tk.Tk()
 
+canvas=tk.Canvas(root,width=600,height=300)
+canvas.grid(columnspan=3,rowspan=3)
+
+#logo
+logo=Image.open('logo.png')
+logo=ImageTk.PhotoImage(logo)
+logo_label=tk.Label(image=logo)
+logo_label.image=logo
+logo_label.grid(column=1,row=0)
+
+#instructions
+instructions = tk.Label(root,text="Select a PDF file on your computer to extract all its texts")
+
+instructions.grid(columnspan=3,column=0,row=1)
+
+#browse button
+browse_text=tk.StringVar()
+browse_btn=tk.Button(root, textvariable=browse_text,bg="#20bebe",fg="white", height=2,width=15)
+browse_text.set('Browse')
+browse_btn.grid(column=1,row=2)
+
+canvas=tk.Canvas(root,width=600,height=250)
+canvas.grid(columnspan=3)
 root.mainloop()
