35edb1252e636cf06f7784f677077bea0b445895
Samanja Cartagena <c.samanja09@Pabs.local>
2023-10-09 09:17:52 -0400

    first commit

---
+++ pdftotext/app.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  9 01:26:11 2023
+
+@author: c.samanja09
+"""
+import tkinter as tk
+import PyPDF2
+from PIL import Image,ImageTk
+
+root = tk.Tk()
+
+root.mainloop()
